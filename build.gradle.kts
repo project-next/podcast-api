aedd98c486c5980af0e97a47fc0113509ecdb79c
Javi Gomez <rtomyj@gmail.com>
2020-05-26 19:37:33 -0500

    Added dependencies for JDBC MySQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,15 +20,21 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
 
 	implementation("com.rometools:rome:${romeToolsVersion}")
 	implementation("com.rometools:rome-modules:${romeToolsVersion}")
+
+	runtimeOnly("mysql:mysql-connector-java")
 }
 
 tasks.withType<Test> {
