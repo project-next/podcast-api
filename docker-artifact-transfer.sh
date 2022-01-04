0f605c2780336cc025336b7116419090ffa0c190
Javi <rtomyj@gmail.com>
2022-01-03 23:22:48 -0600

    Modified file

--- docker-artifact-transfer.sh
+++ docker-artifact-transfer.sh
@@ -1,11 +1,11 @@
 server=$1
 user="ec2-user"
 
-ssh -i "~/.ssh/podcast-api-server.pem" "${user}@${server}" << EOF
+ssh -y -i "~/.ssh/podcast-api-server.pem" "${user}@${server}" << EOF
     mkdir -p api/build/libs
 EOF
 
-sftp -i "~/.ssh/podcast-api-server.pem" "${user}@${server}" << EOF
+sftp -y -i "~/.ssh/podcast-api-server.pem" "${user}@${server}" << EOF
     cd api
     put .env-local.txt
     put docker-compose.yml
