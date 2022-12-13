3c7f66d3edfb9c08550d184900c0ceffd8dc9962
Javi <rtomyj@gmail.com>
2022-12-13 15:43:46 -0600

    cleanup

--- src/main/kotlin/com/rtomyj/podcast/api/Application.kt
+++ src/main/kotlin/com/rtomyj/podcast/api/Application.kt
@@ -1,11 +1,11 @@
 package com.rtomyj.podcast.api
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class Application
 
-fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+fun main() {
+	SpringApplication.run(Application::class.java)
 }
