5d27565d6b8a5acab7e2509a223f751bc6be0623
Javi <rtomyj@gmail.com>
2022-12-13 15:13:31 -0600

    validating input

--- src/main/kotlin/com/rtomyj/podcast/api/controller/StorePodcastDataController.kt
+++ src/main/kotlin/com/rtomyj/podcast/api/controller/StorePodcastDataController.kt
@@ -2,13 +2,16 @@ package com.rtomyj.podcast.api.controller
 
 import com.rtomyj.podcast.api.model.PodcastInfo
 import com.rtomyj.podcast.api.service.PodcastService
+import jakarta.validation.Valid
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Validated
 class StorePodcastDataController {
 	companion object {
 		private val log = LoggerFactory.getLogger(this::class.java.name)
@@ -18,7 +21,7 @@ class StorePodcastDataController {
 	private lateinit var podcastService: PodcastService
 
 	@PostMapping("/new-podcast")
-	fun storeNewPodcast(@RequestBody podcastInfo: PodcastInfo) {
+	fun storeNewPodcast(@Valid @RequestBody podcastInfo: PodcastInfo) {
 		log.info(podcastInfo.podcastId)
 	}
 }
