ba93bed3ca73c202138291825a32fb83a388080b
Javi <8519394+rtomyj@users.noreply.github.com>
2024-01-06 19:23:55 -0600

    Downgrading version to fix manifest reading problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 // main
 val romeToolsVersion = "2.1.0"
-val springBootVersion = "3.2.0"
+val springBootVersion = "3.1.5"
 val jacksonKotlinVersion = "2.16.1"
 val jacksonCoreVersion = "2.16.1"
 val snakeYamlVersion = "2.2"
