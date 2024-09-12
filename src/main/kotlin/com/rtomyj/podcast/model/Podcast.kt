4467cb6a4d9be938379d8b37401079b3be3ac989
Javi <8519394+rtomyj@users.noreply.github.com>
2024-09-12 17:20:32 -0500

    Updating DB location

--- src/main/kotlin/com/rtomyj/podcast/model/Podcast.kt
+++ src/main/kotlin/com/rtomyj/podcast/model/Podcast.kt
@@ -13,69 +13,75 @@ import java.time.LocalDateTime
 import java.util.*
 
 @Entity(name = "podcast")
+@Table(schema = "podcasting")
 @Cacheable
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 data class Podcast(
-	@Id @Column(name = "id", columnDefinition="bpchar", updatable = false, nullable = false) var id: String = UUID.randomUUID().toString()
+    @Id @Column(
+        name = "id",
+        columnDefinition = "bpchar",
+        updatable = false,
+        nullable = false
+    ) var id: String = UUID.randomUUID().toString()
 ) {
-	@NotBlank
-	@Size(max = 50)
-	@Pattern(regexp = "[\\w ]+")
-	@Column(name = "title")
-	lateinit var title: String
+    @NotBlank
+    @Size(max = 50)
+    @Pattern(regexp = "[\\w ]+")
+    @Column(name = "title")
+    lateinit var title: String
 
-	@NotBlank
-	@Size(max = 255)
-	@Pattern(regexp = Constants.URL_REGEX, message = Constants.URL_VALIDATOR_MESSAGE)
-	@Column(name = "link")
-	lateinit var link: String
+    @NotBlank
+    @Size(max = 255)
+    @Pattern(regexp = Constants.URL_REGEX, message = Constants.URL_VALIDATOR_MESSAGE)
+    @Column(name = "link")
+    lateinit var link: String
 
-	@NotBlank
-	@Size(max = 3000)
-	@Column(name = "description")
-	lateinit var description: String
+    @NotBlank
+    @Size(max = 3000)
+    @Column(name = "description")
+    lateinit var description: String
 
-	@NotBlank
-	@Pattern(regexp = "\\w{2}-\\w{2}")
-	@Size(max = 5)
-	@Column(name = "language")
-	lateinit var language: String
+    @NotBlank
+    @Pattern(regexp = "\\w{2}-\\w{2}")
+    @Size(max = 5)
+    @Column(name = "language")
+    lateinit var language: String
 
-	@NotBlank
-	@Size(max = 40)
-	@Column(name = "copyright")
-	lateinit var copyright: String
+    @NotBlank
+    @Size(max = 40)
+    @Column(name = "copyright")
+    lateinit var copyright: String
 
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "last_build_date", nullable = false)
-	@CreationTimestamp
-	lateinit var lastBuildDate: LocalDateTime
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_build_date", nullable = false)
+    @CreationTimestamp
+    lateinit var lastBuildDate: LocalDateTime
 
-	@NotBlank
-	@Email
-	@Size(max = 30)
-	@Column(name = "email")
-	lateinit var email: String
+    @NotBlank
+    @Email
+    @Size(max = 30)
+    @Column(name = "email")
+    lateinit var email: String
 
-	@NotBlank
-	@Size(max = 20)
-	@Column(name = "category")
-	lateinit var category: String
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "category")
+    lateinit var category: String
 
-	@NotBlank
-	@Size(max = 30)
-	@Column(name = "author")
-	lateinit var author: String
+    @NotBlank
+    @Size(max = 30)
+    @Column(name = "author")
+    lateinit var author: String
 
-	var isExplicit: Boolean = true
+    var isExplicit: Boolean = true
 
-	@NotBlank
-	@Size(max = 255)
-	@Pattern(regexp = Constants.URL_REGEX, message = Constants.URL_VALIDATOR_MESSAGE)
-	@Column(name = "image_url")
-	lateinit var imageUrl: String
+    @NotBlank
+    @Size(max = 255)
+    @Pattern(regexp = Constants.URL_REGEX, message = Constants.URL_VALIDATOR_MESSAGE)
+    @Column(name = "image_url")
+    lateinit var imageUrl: String
 
-	override fun toString(): String {
-		return StringBuilder("Podcast ID: $id").append("Podcast Title: $title").append("Podcast Link: $link").toString()
-	}
+    override fun toString(): String {
+        return StringBuilder("Podcast ID: $id").append("Podcast Title: $title").append("Podcast Link: $link").toString()
+    }
 }
