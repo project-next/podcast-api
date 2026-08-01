714167148daba3409619e717f20f1f043d08e7d5
Javi <8519394+rtomyj@users.noreply.github.com>
2026-07-31 21:47:17 -0500

    Utilzing AbstractView to avoid using deprecated AbstractRssFeedView

--- src/main/kotlin/com/rtomyj/podcast/model/RssFeed.kt
+++ src/main/kotlin/com/rtomyj/podcast/model/RssFeed.kt
@@ -1,22 +1,34 @@
 package com.rtomyj.podcast.model
 
 import com.rometools.rome.feed.rss.Channel
-import com.rometools.rome.feed.rss.Item
+import com.rometools.rome.io.WireFeedOutput
 import com.rtomyj.podcast.util.TransformToFeedUtil
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.web.servlet.view.feed.AbstractRssFeedView
+import org.springframework.http.MediaType
+import org.springframework.util.StringUtils
+import org.springframework.web.servlet.view.AbstractView
+import java.io.OutputStreamWriter
 
-class RssFeed(private val podcast: Podcast, private val transformToFeedUtil: TransformToFeedUtil) :
-    AbstractRssFeedView() {
-    override fun buildFeedMetadata(
-        model: MutableMap<String, Any>, feed: Channel, request: HttpServletRequest
-    ) = transformToFeedUtil.populateChannelInfo(feed, podcast)
+class RssFeed(private val podcast: Podcast, private val transformToFeedUtil: TransformToFeedUtil) : AbstractView() {
+    init {
+        contentType = MediaType.APPLICATION_RSS_XML_VALUE
+    }
 
-    override fun buildFeedItems(
+    override fun renderMergedOutputModel(
         model: MutableMap<String, Any>, request: HttpServletRequest, response: HttpServletResponse
-    ): ArrayList<Item> {
-        val episodes = podcast.episodes.map { episode -> transformToFeedUtil.episodeToFeed(episode) }
-        return episodes as ArrayList<Item>
+    ) {
+        val channel = Channel("rss_2.0")
+        transformToFeedUtil.populateChannelInfo(channel, podcast)
+        channel.items = podcast.episodes.map { episode -> transformToFeedUtil.episodeToFeed(episode) }
+
+        setResponseContentType(request, response)
+        if (!StringUtils.hasText(channel.encoding)) {
+            channel.encoding = "UTF-8"
+        }
+
+        val out = response.outputStream
+        WireFeedOutput().output(channel, OutputStreamWriter(out, channel.encoding))
+        out.flush()
     }
 }
